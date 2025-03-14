a383e24b08026b45831f371276d3965bdf4a45a6
Sergeant Svyatskiy <sithserega@gmail.com>
2025-03-14 18:00:51 +0300

    add class

---
+++ src/main/java/objects/Contact.java
@@ -0,0 +1,18 @@
+package objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+
+    private String contactFirstName;
+    private String contactLastName;
+    private String salutation;
+    private String contactAccountName;
+    private String description;
+    private String phone;
+}
